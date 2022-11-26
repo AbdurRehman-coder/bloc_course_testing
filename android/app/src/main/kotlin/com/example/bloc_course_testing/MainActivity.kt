3173154c407a43c923d2edba2264521fdcfcffec
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2022-11-27 00:06:40 +0500

    step-1

---
+++ android/app/src/main/kotlin/com/example/bloc_course_testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_course_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bloc_random_name.dart
@@ -0,0 +1,100 @@
+
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
+
+import 'package:flutter/material.dart';
+
+List<String> names = [
+  'Foo',
+  'Bar',
+  'Baz'
+];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+/// Cubit class
+class RandomNameCubit extends Cubit<String?>{
+  /// constructor
+  RandomNameCubit() : super(null);
+
+  void pickedRandomName() => emit(names.getRandomElement());
+}
+
+class RandomNameBloc extends StatefulWidget {
+  const RandomNameBloc({Key? key}) : super(key: key);
+
+  @override
+  State<RandomNameBloc> createState() => _RandomNameBlocState();
+}
+
+class _RandomNameBlocState extends State<RandomNameBloc> {
+
+  late final RandomNameCubit nameCubit;
+
+  /// Todo: Just did some stream practice
+  // late StreamController<String?> streamController;
+  // late Stream<String?> stream;
+  //
+  // void pickedRandomName() {
+  //   streamController.add(names.elementAt(math.Random().nextInt(names.length)));
+  // }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    nameCubit = RandomNameCubit();
+
+    /// Stream Practice
+    // streamController = StreamController.broadcast();
+    // stream = streamController.stream;
+
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    nameCubit.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cubit Testing'),
+      ),
+      body: StreamBuilder<String?>(
+        stream: nameCubit.stream,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          final button = TextButton(
+              onPressed: () => nameCubit.pickedRandomName(),
+              child: const Text('Get random name'),);
+          /// switch for different connection state
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+              return Text('No data');
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Column(
+                children: [
+                  Text('Name: ${snapshot.data}'),
+                  button,
+                ],
+              );
+            case ConnectionState.done:
+              return Text('Done');
+          }
+        },
+
+      ),
+    );
+  }
+}
